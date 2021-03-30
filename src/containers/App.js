8b1755eb85caa0b9d969f4a6c95eb5c0a99d8354
Benoit Rion <benoitrion@gmail.com>
2021-03-30 23:20:20 +0200

    setup robofriends project

---
+++ src/containers/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect}  from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import './App.css'
+import Scroll from "../components/Scroll";
+
+const App = () => {
+    const [ robots, setRobots ] = useState([]);
+    const [ searchField, setSearchField] = useState('');
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users))
+    });
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
+    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    return !robots.length ?
+        <h1 className='tc f1'>Loading robots...</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        )
+}
+
+export default App;
